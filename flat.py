6caa33ed4f513b3ef9179a98f03efdb2d180e0f3
Nambukeerthi <nambu935@gmail.com>
2025-01-15 00:15:42 +0530

    Update flat.py

--- flat.py
+++ flat.py
@@ -43,8 +43,8 @@ if selected == "Home":
         rar_file_content = os.path.exists("flat_data/finalflat.rar")
         rarfile.UNRAR_TOOL = "requirements.txt"
         new_file_name = 'finalflat.csv'
-       #with io.BytesIO(rar_file_content) as file_obj:
-        with rarfile.RarFile(rar_file_content) as rf:
+       with io.BytesIO(rar_file_content) as file_obj:
+         with rarfile.RarFile(file_obj) as rf:
              rf.extractall('.')
              with rf.open( new_file_name , 'r') as file:  
                   csv_data = csv.reader(file)
